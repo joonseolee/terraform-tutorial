492206d905f0bbd300ffba799d931bf095d29986
Joonseo Lee <niceweather94@gmail.com>
2023-12-23 15:38:19 +0900

    feat: for 구문 추가

--- book/terraform-starter/03.start/main.tf
+++ book/terraform-starter/03.start/main.tf
@@ -44,3 +44,38 @@ resource "local_file" "name_file" {
   content = "looping!"
   filename = "${path.module}/abc-name-${element(var.names, count.index)}.txt"
 }
+
+variable "members" {
+  type = map(object({
+    role = string
+  }))
+
+  default = {
+    "ab" = {
+      role = "member", group = "dev"
+    }
+    "cd" = {
+      role = "admin", group = "dev"
+    }
+    "ef" = {
+      role = "member", group = "ops"
+    }
+  }
+}
+
+output "A_to_tuple" {
+  value = [for k, v in var.members: "${k} is ${v.role}"]
+}
+
+output "B_get_only_role" {
+  value = {
+    for name, user in var.members: name => user.role
+    if user.role == "admin"
+  }
+}
+
+output "C_group" {
+  value = {
+    for name, user in var.members: user.role => name...
+  }
+}
