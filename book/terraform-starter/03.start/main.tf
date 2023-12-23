f5b7973e75c7ab97e0c57fcbc193113df2b037b8
Joonseo Lee <niceweather94@gmail.com>
2023-12-23 14:22:27 +0900

    feat: def 파일 제거 스크립트로 수정

--- book/terraform-starter/03.start/main.tf
+++ book/terraform-starter/03.start/main.tf
@@ -2,8 +2,3 @@ resource "local_file" "abc" {
     content = "abc!"
     filename = "${path.module}/abc.txt"
 }
-
-resource "local_file" "def" {
-    content = "def!"
-    filename = "${path.module}/def.txt"
-}
