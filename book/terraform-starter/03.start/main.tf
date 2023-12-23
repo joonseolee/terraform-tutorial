436f4d5d257a2c7b7286eae04a2e7930042a6760
Joonseo Lee <niceweather94@gmail.com>
2023-12-23 15:04:48 +0900

    feat: 생명주기 precondition 적용

--- book/terraform-starter/03.start/main.tf
+++ book/terraform-starter/03.start/main.tf
@@ -1,10 +1,21 @@
+variable "file_name" {
+  default = "step0.txt"
+}
+
 resource "local_file" "abc" {
-  content  = "abc!"
-  filename = "${path.module}/abc.txt"
+  content  = "lifecycle~~222!"
+  filename = "${path.module}/${var.file_name}"
+
+  lifecycle {
+    precondition {
+      condition     = var.file_name == "step6.txt"
+      error_message = "file name is not \"step06.txt\""
+    }
+  }
 }
 
 resource "local_file" "def" {
   depends_on = [local_file.abc]
-  content    = local_file.abc.content
+  content    = "def!"
   filename   = "${path.module}/def.txt"
 }
