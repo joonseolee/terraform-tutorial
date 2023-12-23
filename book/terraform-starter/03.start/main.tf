df5c4d56f4191159263b2e0ae8210786a746a982
Joonseo Lee <niceweather94@gmail.com>
2023-12-23 14:53:35 +0900

    feat: 다른 리소스 내용 참조 추가

--- book/terraform-starter/03.start/main.tf
+++ book/terraform-starter/03.start/main.tf
@@ -2,3 +2,8 @@ resource "local_file" "abc" {
   content  = "abc!"
   filename = "${path.module}/abc.txt"
 }
+
+resource "local_file" "def" {
+  content = local_file.abc.content
+  filename = "${path.module}/def.txt"
+}
